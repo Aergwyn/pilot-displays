98e341f3da2ae1e8f033218d6d391ce7c272056c
Aergwyn <aergwyn@t-online.de>
2021-09-21 13:29:29 +0200

    flip heading upside down (in a way)

--- HMD/classes/hmd_heading.hpp
+++ HMD/classes/hmd_heading.hpp
@@ -1,7 +1,7 @@
 class Heading_Group
 {
 	clipTL[] = { 0.03, 0 };
-	clipBR[] = { 0.97, 0.09 };
+	clipBR[] = { 0.97, 0.075 };
 	condition = "user10";
 	type="group";
 
@@ -9,11 +9,12 @@ class Heading_Group
 	{
 		align = "center";
 		horizontal = 1;
-		majorLineEach = 3;
+		majorLineEach = 2;
 		NeverEatSeaWeed = 1;
 		numberEach = 6;
 		scale = 1;
 		source = "heading";
+		sourceLength = 3;
 		sourceScale = 1;
 		step = 15;
 		stepSize = 0.04;
@@ -26,12 +27,12 @@ class Heading_Group
 		width = 2;
 		lineXleft = 0.01;
 		lineYright = 0.03;
-		lineXleftMajor = 0.01;
-		lineYrightMajor =0.02;
+		lineXleftMajor = 0.02;
+		lineYrightMajor =0.03;
 
 		#define SIZE 0.03
 		#define X_OFFSET -0.0027
-		#define Y_OFFSET 0.018
+		#define Y_OFFSET -0.0075
 
 		pos[] = { 0 + X_OFFSET, 0 + Y_OFFSET };
 		right[] = { 0 + X_OFFSET + SIZE, 0 + Y_OFFSET };
@@ -43,15 +44,20 @@ class Heading_Group
 		type = "line";
 		width = 2;
 
+		#define WIDTH 0.05
+		#define HEIGHT 0.02
+		#define X_OFFSET 0.5
+		#define Y_OFFSET 0.044
+
 		points[] =
 		{
-			{ { 0.5, 0.045 }, 1 },
-			{ { 0.5, 0.055 }, 1 },
+			{ { X_OFFSET, Y_OFFSET - (HEIGHT / 2) }, 1 },
+			{ { X_OFFSET, Y_OFFSET }, 1 },
 			{ },
-			{ { 0.475, 0.065 }, 1 },
-			{ { 0.475, 0.055 }, 1 },
-			{ { 0.525, 0.055 }, 1 },
-			{ { 0.525, 0.065 }, 1 }
+			{ { X_OFFSET - (WIDTH / 2), Y_OFFSET + (HEIGHT / 2) }, 1 },
+			{ { X_OFFSET - (WIDTH / 2), Y_OFFSET }, 1 },
+			{ { X_OFFSET + (WIDTH / 2), Y_OFFSET }, 1 },
+			{ { X_OFFSET + (WIDTH / 2), Y_OFFSET + (HEIGHT / 2) }, 1 }
 		};
 	};
 	class Heading_Number
@@ -66,7 +72,7 @@ class Heading_Group
 
 		#define SIZE 0.03
 		#define X_OFFSET -0.003
-		#define Y_OFFSET 0.054
+		#define Y_OFFSET 0.044
 
 		pos[] = { { 0.5 + X_OFFSET, 0 + Y_OFFSET }, 1 };
 		right[] = { { 0.5 + X_OFFSET + SIZE, 0 + Y_OFFSET }, 1 };
